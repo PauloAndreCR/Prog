a2b1c6a224fa75adb2975d89824640d7e1fdc207
Paulo André Castilho Rosa <plandrecastilho@hotmail.com>
2024-10-09 14:18:55 +0000

    update

--- Prog 2/Matriz produto/main.c
+++ Prog 2/Matriz produto/main.c
@@ -1,42 +1,42 @@
 // #include <stdio.h> // PRODUTO ENTRE MATRIZES
 
-// int main(void) {
-//     int matrizA[3][2];
-//     int matrizB[2][3];
-//     int matrizC[3][3] = {0};
+ /*int main(void) {
+    int matrizA[3][2];
+    int matrizB[2][3];
+    int matrizC[3][3] = {0};
 
-//     for(int i = 0; i < 3; i++) { // Pegar os valores da Matriz A
-//         for(int j = 0; j < 2; j++) {
-//             printf("Valor da %d linha e %d coluna da matriz A: ", i+1, j+1);
-//             scanf("%d", &matrizA[i][j]);
-//         }
-//         printf("\n");
-//     }
-//     for(int i = 0; i < 2; i++) { // Pegar os valores da Matriz B
-//         for(int j = 0; j < 3; j++) {
-//             printf("Valor da %d linha e %d coluna da matriz B: ", i+1, j+1);
-//             scanf("%d", &matrizB[i][j]);
-//         }
-//         printf("\n");
-//     }
+     for(int i = 0; i < 3; i++) { // Pegar os valores da Matriz A
+         for(int j = 0; j < 2; j++) {
+             printf("Valor da %d linha e %d coluna da matriz A: ", i+1, j+1);
+             scanf("%d", &matrizA[i][j]);
+         }
+         printf("\n");
+     }
+     for(int i = 0; i < 2; i++) { // Pegar os valores da Matriz B
+         for(int j = 0; j < 3; j++) {
+             printf("Valor da %d linha e %d coluna da matriz B: ", i+1, j+1);
+             scanf("%d", &matrizB[i][j]);
+         }
+         printf("\n");
+     }
 
-//     for(int i = 0; i < 3; i++) { // Produto da matriz A com a matriz B
-//         for(int j = 0; j < 3; j++) {
-//             for(int k = 0; k < 2; k++) {
-//                 matrizC[i][j] = matrizC[i][j] + matrizA[i][k] * matrizB[k][j];
-//             }
-//         }
-//     }
+     for(int i = 0; i < 3; i++) { // Produto da matriz A com a matriz B
+         for(int j = 0; j < 3; j++) {
+             for(int k = 0; k < 2; k++) {
+                 matrizC[i][j] = matrizC[i][j] + matrizA[i][k] * matrizB[k][j];
+             }
+         }
+     }
 
-//     for(int i = 0; i < 3; i++) { // Printar a matriz resultante do produto
-//         for(int j = 0; j < 3; j++) {
-//             printf("[ %d ]", matrizC[i][j]);
-//         }
-//         printf("\n");
-//     }
+     for(int i = 0; i < 3; i++) { // Printar a matriz resultante do produto
+         for(int j = 0; j < 3; j++) {
+             printf("[ %d ]", matrizC[i][j]);
+         }
+         printf("\n");
+     }
 
-//     return 0;
-// } 
+     return 0;
+ } */
 
 #include <stdio.h>
 
